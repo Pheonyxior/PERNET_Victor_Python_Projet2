3a97f1a4b92a4183b43c39ae0bc59da7d54281ca
Victor PERNET <victor@av-connexion.fr>
2024-10-18 07:54:22 +0200

    print books from category

--- scraper.py
+++ scraper.py
@@ -3,15 +3,18 @@ from bs4 import BeautifulSoup
 import csv
 from urllib.parse import urljoin
 
-category_url = 'http://books.toscrape.com/catalogue/category/books/young-adult_21/index.html' #'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-category_url2 = 'http://books.toscrape.com/catalogue/category/books/classics_6/index.html'
+site_to_scrape = 'http://books.toscrape.com/index.html'
+
+url1 = 'http://books.toscrape.com/catalogue/category/books/young-adult_21/index.html' #'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+url2 = 'http://books.toscrape.com/catalogue/category/books/classics_6/index.html'
 catalogue_url = 'http://books.toscrape.com/catalogue/'
 
 def get_book_data(url, category):
     page = rq.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    title = soup.find('title').text
+    title = soup.find('h1').text
+    print(title)
     thumbnail = soup.find('img')
     
     trs = soup.find_all('tr')
@@ -29,7 +32,10 @@ def get_book_data(url, category):
         elif th == 'Availability':
             num_available = td
     
-    description = soup.find('meta', attrs = {'name': 'description'}).get('content')
+    description = soup.find('meta', attrs = {'name': 'description'}).get('content').strip()
+    #print(description)
+    #print("FOUND CHARA")
+    
     review_rating = soup.find_all('p')[2]
     if review_rating.has_attr('class'):
         review_rating = review_rating['class'][1]
@@ -60,20 +66,20 @@ def get_books_from_page(url, soup):
         book_urls.append(href)
     return book_urls
 
-
-if __name__ == "__main__":
+def get_books_from_category(url, category):
     book_datas = []
-
-    current_url = category_url2
+    
+    current_url = url
     page = rq.get(current_url)
     soup = BeautifulSoup(page.text, 'html.parser')
     next = soup.find('li', class_ = 'next')
     suf = 'index.html'
     
+    # Loops as long as a next button for the next page is found
     while next:
         print(current_url)
         for url in get_books_from_page(current_url, soup):
-            book_datas.append(get_book_data(url, "Young Adult"))
+            book_datas.append(get_book_data(url, category))
         
         href = next.find('a')['href']
         current_url = current_url.replace(suf, href)
@@ -81,14 +87,36 @@ if __name__ == "__main__":
         soup = BeautifulSoup(page.text, 'html.parser')
         suf = href
         next = soup.find('li', class_ = 'next')
-        
+
+    # Executes at least once for the page where no next is found, meaning the last one.
     print(current_url)
     for url in get_books_from_page(current_url, soup):
-            book_datas.append(get_book_data(url, "Young Adult"))
+            book_datas.append(get_book_data(url, category))
+    
+    return book_datas
 
-    with open("test.csv", mode="w",newline='', encoding= 'utf-8') as csv_file:
+
+if __name__ == "__main__":
+
+    page = rq.get(site_to_scrape)
+    soup = BeautifulSoup(page.text, 'html.parser')
+
+    sides = soup.find('div', attrs= {'class': 'side_categories'})
+    categories = sides.find_all('li')
+    for i in range(1, len(categories)):
+        print(categories[i].text.strip())
+
+    book_datas = []
+
+    book_datas.extend(get_books_from_category(url1, "Young Adults"))
+    book_datas.extend(get_books_from_category(url2, "Classics"))
+
+    #extend() Add the elements of a list (or any iterable), to the end of the current list
+
+    #csv_file =  open("test.csv", mode="w",newline='', encoding= 'utf-8')
+    with open("test.csv", mode="w", encoding= 'utf-8') as csv_file:
         # csv_file = open("test.csv", mode="w",newline='')
-        csv_file.write('sep=,\n')
+        #csv_file.write('sep=,\n')
 
         data = {
             'product_page_url': 'product_page_url', 
@@ -104,7 +132,7 @@ if __name__ == "__main__":
         }
 
         fieldnames = data.keys()
-        writer = csv.DictWriter(csv_file, fieldnames= fieldnames)
+        writer = csv.DictWriter(csv_file, fieldnames= fieldnames, delimiter=';')
         writer.writerow(data)
 
         writer.writerows(book_datas)
